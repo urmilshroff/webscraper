fab9cb6edc0020c18c4213454ba7a7ed4f6eb8a1
urmilshroff <urmilshroff@gmail.com>
2018-11-30 00:42:37 +0530

    More info

--- github.py
+++ github.py
@@ -8,5 +8,10 @@ res=requests.get(user_link)
 soup=bs4.BeautifulSoup(res.text,"lxml")
 
 name=soup.select(".p-name.vcard-fullname.d-block.overflow-hidden")
+count=soup.select(".Counter")
 
 print("Summary for GitHub user {} (aka {}):".format(user_id,name[0].text))
+print("Number of repositories: {}".format(count[0].text.strip()))
+print("Number of stars: {}".format(count[1].text.strip()))
+print("Number of followers: {}".format(count[2].text.strip()))
+print("Number of following: {}".format(count[3].text.strip()))
