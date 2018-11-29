476636fa8ca4dbc22b1c637ef38b6a7389a33acf
urmilshroff <urmilshroff@gmail.com>
2018-11-30 00:32:47 +0530

    Name detection

--- github.py
+++ github.py
@@ -6,3 +6,7 @@ user_link="https://github.com/"+user_id
 
 res=requests.get(user_link)
 soup=bs4.BeautifulSoup(res.text,"lxml")
+
+name=soup.select(".p-name.vcard-fullname.d-block.overflow-hidden")
+
+print("Summary for GitHub user {} (aka {}):".format(user_id,name[0].text))
