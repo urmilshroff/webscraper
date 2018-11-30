0c21546a7f154d2e8d8eee1206d1c29fff03c991
urmilshroff <urmilshroff@gmail.com>
2018-12-01 01:59:22 +0530

    Added error handling

--- github.py
+++ github.py
@@ -11,25 +11,40 @@ soup=bs4.BeautifulSoup(res.text,"lxml")
 name=soup.select(".p-name.vcard-fullname.d-block.overflow-hidden")
 count=soup.select(".Counter")
 
-contribs=soup.select(".f4.text-normal.mb-2")
-contribs=contribs[1].text
-contribs=contribs[:-38]
+try:
+    contribs=soup.select(".f4.text-normal.mb-2")
+    contribs=contribs[1].text
+    contribs=contribs[:-38]
+except IndexError:
+    print("Error: missing or broken info!")
 
-max_contribs={}
-for item in soup.findAll("rect",{"class":"day"}):
-    date=item.get("data-date")
-    max_contribs[date]=int(item.get("data-count"))
-max_contribs=sorted(max_contribs.items(),key=operator.itemgetter(1),reverse=True)
-max_num=max_contribs[0][1:]
-max_date=max_contribs[0][:1]
+try:
+    max_contribs={}
+    for item in soup.findAll("rect",{"class":"day"}):
+        date=item.get("data-date")
+        max_contribs[date]=int(item.get("data-count"))
+    max_contribs=sorted(max_contribs.items(),key=operator.itemgetter(1),reverse=True)
+    max_num=max_contribs[0][1:]
+    max_date=max_contribs[0][:1]
+except IndexError:
+    print("Error: missing or broken info!")
 
-print("\nSummary for GitHub user {} (aka {}):".format(user_id,name[0].text))
+try:
+    print("\nSummary for GitHub user {} (aka {}):".format(user_id,name[0].text))
+except IndexError:
+    print("Error: missing or broken info!")
 
-print("\nNumber of repositories: {}".format(count[0].text.strip()))
-print("Number of starred items: {}".format(count[1].text.strip()))
-print("Number of followers: {}".format(count[2].text.strip()))
-print("Number of following: {}".format(count[3].text.strip()))
+try:
+    print("\nNumber of repositories: {}".format(count[0].text.strip()))
+    print("Number of starred items: {}".format(count[1].text.strip()))
+    print("Number of followers: {}".format(count[2].text.strip()))
+    print("Number of following: {}".format(count[3].text.strip()))
+except IndexError:
+    print("Error: missing or broken info!")
 
-print("\nMost contributions in a single day: {}".format(max_num[:2]))
-print("Date of most contributions in a day: {}".format(max_date[:3]))
-print("Total contributions in the last year: {}".format(contribs.strip()))
+try:
+    print("\nMost contributions in a single day: {}".format(max_num[:2]))
+    print("Date of most contributions in a day: {}".format(max_date[:3]))
+    print("Total contributions in the last year: {}".format(contribs.strip()))
+except IndexError:
+    print("Error: missing or broken info!")
--- twitter.py
+++ twitter.py
@@ -10,18 +10,33 @@ soup=bs4.BeautifulSoup(res.text,"lxml")
 
 name=soup.select(".ProfileHeaderCard-nameLink.u-textInheritColor.js-nav")
 
-count=[]
-for item in soup.findAll("span",{"class":"ProfileNav-value"}):
-    count.append(item.get("data-count"))
+try:
+    count=[]
+    for item in soup.findAll("span",{"class":"ProfileNav-value"}):
+        count.append(item.get("data-count"))
+except IndexError:
+    print("Error: missing or broken info!")
 
-join=soup.select(".ProfileHeaderCard-joinDateText.js-tooltip.u-dir")
-joined=join[0].get("title")
+try:
+    join=soup.select(".ProfileHeaderCard-joinDateText.js-tooltip.u-dir")
+    joined=join[0].get("title")
+except IndexError:
+    print("Error: missing or broken info!")
 
-print("\nSummary for Twitter user @{} (aka {}):".format(user_id,name[0].text))
+try:
+    print("\nSummary for Twitter user @{} (aka {}):".format(user_id,name[0].text))
+except IndexError:
+    print("Error: missing or broken info!")
 
-print("\nNumber of Tweets: {}".format(count[0]))
-print("Number of following: {}".format(count[1]))
-print("Number of followers: {}".format(count[2]))
-print("Number of likes: {}".format(count[3]))
+try:
+    print("\nNumber of Tweets: {}".format(count[0]))
+    print("Number of following: {}".format(count[1]))
+    print("Number of followers: {}".format(count[2]))
+    print("Number of likes: {}".format(count[3]))
+except IndexError:
+    print("Error: missing or broken info!")
 
-print("\nJoined Twitter at: {}".format(joined))
+try:
+    print("\nJoined Twitter at: {}".format(joined))
+except IndexError:
+    print("Error: missing or broken info!")
