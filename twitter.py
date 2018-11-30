0bb51b643a32315b8519e003218f928efd6a0745
urmilshroff <urmilshroff@gmail.com>
2018-12-01 01:41:55 +0530

    Added more info

--- twitter.py
+++ twitter.py
@@ -10,4 +10,18 @@ soup=bs4.BeautifulSoup(res.text,"lxml")
 
 name=soup.select(".ProfileHeaderCard-nameLink.u-textInheritColor.js-nav")
 
-print("\nSummary for Twitter user {} (aka {}):".format(user_id,name[0].text))
+count=[]
+for item in soup.findAll("span",{"class":"ProfileNav-value"}):
+    count.append(item.get("data-count"))
+
+join=soup.select(".ProfileHeaderCard-joinDateText.js-tooltip.u-dir")
+joined=join[0].get("title")
+
+print("\nSummary for Twitter user @{} (aka {}):".format(user_id,name[0].text))
+
+print("\nNumber of Tweets: {}".format(count[0]))
+print("Number of following: {}".format(count[1]))
+print("Number of followers: {}".format(count[2]))
+print("Number of likes: {}".format(count[3]))
+
+print("\nJoined Twitter at: {}".format(joined))
